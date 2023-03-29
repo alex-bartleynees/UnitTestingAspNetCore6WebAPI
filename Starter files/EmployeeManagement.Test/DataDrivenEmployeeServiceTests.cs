9e1cb6661465f1f4d4feea966b2fabea347a9be2
Alexander Nees <alexbartleynees@gmail.com>
2023-03-29 15:50:31 +1300

    Finish data driven tests section

--- Starter files/EmployeeManagement.Test/DataDrivenEmployeeServiceTests.cs
+++ Starter files/EmployeeManagement.Test/DataDrivenEmployeeServiceTests.cs
@@ -21,6 +21,22 @@ namespace EmployeeManagement.Test
             _employeeServiceFixture = employeeServiceFixture;
         }
 
+        [Theory]
+        [InlineData("1fd115cf-f44c-4982-86bc-a8fe2e4ff83e")]
+        [InlineData("37e03ca7-c730-4351-834c-b66f280cdb01")]
+        public void CreateInternalEmployee_InternalEmployeeCreated_MustHaveAttendedSecondObligatoryCourse_WithPredicate(Guid courseId)
+        {
+            // Arrange 
+
+            // Act
+            var internalEmployee = _employeeServiceFixture.EmployeeService
+                .CreateInternalEmployee("Brooklyn", "Cannon");
+
+            // Assert
+            Assert.Contains(internalEmployee.AttendedCourses,
+                course => course.Id == courseId);
+        }
+
         [Fact]
         public void CreateInternalEmployee_InternalEmployeeCreated_MustHaveAttendedFirstObligatoryCourse()
         {
@@ -117,6 +133,58 @@ namespace EmployeeManagement.Test
                 handler => _employeeServiceFixture.EmployeeService.EmployeeIsAbsent += handler,
                 handler => _employeeServiceFixture.EmployeeService.EmployeeIsAbsent -= handler,
                 () => _employeeServiceFixture.EmployeeService.NotifyOfAbsence(internalEmployee));
+        }
+
+        [Theory]
+        [MemberData(nameof(ExampleTestDataForGiveRaise_WithMethod), 2)]
+        public async Task GiveRaise_RaiseGiven_EmployeeMinimumRaiseGivenMatchesValue(
+            int raiseGiven, bool expectedValueForMinimumRaiseGiven)
+        {
+            // Arrange
+            var internalEmployee = new InternalEmployee(
+                "Brooklyn", "Cannon", 5, 3000, false, 1
+                );
+
+            // Act
+            await _employeeServiceFixture.EmployeeService.GiveRaiseAsync(internalEmployee, raiseGiven);
+
+            // Assert
+            Assert.Equal(expectedValueForMinimumRaiseGiven, internalEmployee.MinimumRaiseGiven);
+        }
+
+        public static IEnumerable<object[]> ExampleTestDataForGiveRaise_WithProperty
+        {
+            get
+            {
+                return new List<object[]>
+                {
+                    new object[] {100, true},
+                    new object[] {200, false},
+                };
+            }
+        }
+
+        public static TheoryData<int, bool> StronglyTypedExampleTestDataForGiveRaise_WithProperty
+        {
+            get
+            {
+                return new TheoryData<int, bool>()
+                {
+                    {100, true },
+                    {200, false }
+                };
+            }
+        }
+
+        public static IEnumerable<object[]> ExampleTestDataForGiveRaise_WithMethod(int testDataInstancesToProvide)
+        {
+            var testData = new List<object[]>
+            {
+                new object[] {100, true},
+                new object[] {200, false},
+            };
+
+            return testData.Take(testDataInstancesToProvide);
         }
     }
 }
---
+++ Starter files/EmployeeManagement.Test/TestData/StronglyTypedEmployeeServiceTestData.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeManagement.Test.TestData
+{
+    public class StronglyTypedEmployeeServiceTestData : TheoryData<int, bool>
+    {
+        public StronglyTypedEmployeeServiceTestData()
+        {
+            Add(100, true)
+        }
+    }
+}
