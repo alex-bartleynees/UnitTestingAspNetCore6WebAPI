fd37721c48343bafbd83b2a6268e28024cf675cd
Alexander Nees <alexbartleynees@gmail.com>
2023-05-08 09:01:51 +1200

    Finish middleware/services testing section

---
+++ Starter files/EmployeeManagement.Test/CheckShowStatisticsHeadersTests.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EmployeeManagement.ActionFilters;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace EmployeeManagement.Test
+{
+    public class CheckShowStatisticsHeadersTests
+    {
+        [Fact]
+        public void OnActionExecuting_InvokeWithoutShowStatisticsHeader_ReturnsBackBadRequest()
+        {
+            // Arrange
+            var checkShowStatisticsHeaderActionFilter = new CheckShowStatisticsHeader();
+
+            var httpContext = new DefaultHttpContext();
+
+            var actionContext = new ActionContext(httpContext, new(), new(), new());
+            var actionExecutingContext = new ActionExecutingContext(actionContext, 
+                new List<IFilterMetadata>(), 
+                new Dictionary<string, object?>(),
+                controller: null);
+
+            // Act
+
+            checkShowStatisticsHeaderActionFilter.OnActionExecuting(actionExecutingContext);
+
+            // Assert
+
+            Assert.IsType<BadRequestResult>(actionExecutingContext.Result);
+        }
+    }
+}
---
+++ Starter files/EmployeeManagement.Test/ServiceCollectionTests.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EmployeeManagement.DataAccess.Services;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace EmployeeManagement.Test
+{
+    public class ServiceCollectionTests
+    {
+        [Fact]
+        public void RegisterDataService_Execute_DataServicesAreRegistered()
+        {
+            // Arrange
+            IDictionary<string, string> inMemoryConfigSettings = new Dictionary<string, string>()
+            {
+                { "ConnectionStrings:EmployeeManagementDB", "AnyValueWillDo" }
+            };
+                
+            var serviceCollection = new ServiceCollection();
+            var configuration = new ConfigurationBuilder()
+                   .AddInMemoryCollection(inMemoryConfigSettings).Build();
+             
+            // Act 
+            serviceCollection.RegisterDataServices(configuration);
+            var serviceProvider = serviceCollection.BuildServiceProvider();
+
+            // Assert
+            Assert.NotNull(serviceProvider.GetService<IEmployeeManagementRepository>());
+            Assert.IsType<EmployeeManagementRepository>(serviceProvider.GetService<IEmployeeManagementRepository>());
+        }
+    }
+}
